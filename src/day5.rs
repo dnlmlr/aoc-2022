d08d970c5ef5502c9be4870ab87d157802b1974c
Daniel M <danielm-github@dnml.de>
2022-12-05 15:44:53 +0100

    Day 5 part 2 optimized to basically C

--- src/day5.rs
+++ src/day5.rs
@@ -56,102 +56,74 @@ pub fn day5_part1(dataset: &[u8]) -> String {
         .collect()
 }
 
-#[derive(Debug, Clone)]
-struct Stack {
-    data: [u8; 64],
-    idx: usize,
-}
-
-impl Stack {
-    fn new() -> Self {
-        Self {
-            data: [0; 64],
-            idx: 0,
-        }
-    }
-
-    fn reverse(&mut self) {
-        self.data[..self.idx].reverse();
-    }
-
-    fn push(&mut self, val: u8) {
-        self.data[self.idx] = val;
-        self.idx += 1;
-    }
-
-    fn pop(&mut self) -> u8 {
-        self.idx -= 1;
-        self.data[self.idx]
-    }
-
-    fn last(&self) -> Option<u8> {
-        (self.idx > 0).then(|| self.data[self.idx - 1])
-    }
-}
-
 #[aoc(day5, part1, optimized)]
 pub fn day5_part1_optimized(dataset: &[u8]) -> String {
-    let mut stacks = [
-        Stack::new(),
-        Stack::new(),
-        Stack::new(),
-        Stack::new(),
-        Stack::new(),
-        Stack::new(),
-        Stack::new(),
-        Stack::new(),
-        Stack::new(),
-    ];
+    let stacks = [0_u8; 64 * 9];
+    let stack_idx = [0_usize; 9];
 
-    let mut i = 0;
+    unsafe {
+        let s_ptr = stacks.as_ptr() as *mut u8;
+        let sidx_ptr = stack_idx.as_ptr() as *mut usize;
 
-    dataset
-        .array_chunks::<36>()
-        .take_while(|line| line[1] != b'1')
-        .for_each(|line| {
-            i += 36;
-            line.array_chunks::<4>().enumerate().for_each(|(k, c)| {
-                if c[1] != b' ' {
-                    stacks[k].push(c[1]);
+        let mut ds_ptr = dataset.as_ptr().add(1);
+        let ds_ptr_end = dataset.as_ptr_range().end;
+
+        let mut k = 7;
+        while *ds_ptr != b'1' {
+            for i in 0..9 {
+                let val = *ds_ptr;
+                if val != b' ' {
+                    *s_ptr.add((i << 6) + k) = val;
+                    *sidx_ptr.add(i) += 1;
                 }
-            });
-        });
+                ds_ptr = ds_ptr.add(4);
+            }
+            k -= 1;
+        }
+        ds_ptr = ds_ptr.add(36);
 
-    for stack in &mut stacks {
-        stack.reverse();
+        while ds_ptr < ds_ptr_end {
+            ds_ptr = ds_ptr.add(5);
+
+            let cd0 = *ds_ptr;
+            let cd1 = *ds_ptr.add(1);
+
+            let count;
+            if cd1 == b' ' {
+                ds_ptr = ds_ptr.add(7);
+                count = (cd0 - b'0') as usize;
+            } else {
+                ds_ptr = ds_ptr.add(8);
+                count = cd0 as usize * 10 + cd1 as usize - (b'0' as usize * 11);
+            }
+
+            let from = *ds_ptr as usize - b'0' as usize - 1;
+            let to = *ds_ptr.add(5) as usize - b'0' as usize - 1;
+
+            ds_ptr = ds_ptr.add(7);
+
+            for _ in 0..count {
+                *sidx_ptr.add(from) -= 1;
+
+                *s_ptr.add((to << 6) + *sidx_ptr.add(to)) =
+                    *s_ptr.add((from << 6) + *sidx_ptr.add(from));
+
+                *sidx_ptr.add(to) += 1;
+            }
+        }
     }
 
-    i += 37;
-    while i < dataset.len() {
-        i += 5;
+    let mut out = String::with_capacity(9);
 
-        let count;
-        if unsafe { *dataset.get_unchecked(i + 1) } == b' ' {
-            count = (unsafe { *dataset.get_unchecked(i) } - b'0') as usize;
-            i += 7;
-        } else {
-            count = unsafe { *dataset.get_unchecked(i) } as usize * 10
-                + unsafe { *dataset.get_unchecked(i + 1) } as usize
-                - (b'0' as usize * 11);
-            i += 8;
+    for i in 0..9 {
+        if stack_idx[i] == 0 {
+            continue;
         }
-
-        let from = (unsafe { *dataset.get_unchecked(i) } - b'0') as usize - 1;
-        let to = (unsafe { *dataset.get_unchecked(i + 5) } - b'0') as usize - 1;
-
-        i += 7;
-
-        for _ in 0..count {
-            let val = stacks[from].pop();
-            stacks[to].push(val);
-        }
+        let val = stacks[i * 64 + stack_idx[i] - 1];
+        out.push(val as char);
     }
 
-    stacks
-        .into_iter()
-        .filter_map(|stack| stack.last())
-        .map(|c| c as char)
-        .collect::<String>()
+    out
 }
 
 #[cfg(test)]
